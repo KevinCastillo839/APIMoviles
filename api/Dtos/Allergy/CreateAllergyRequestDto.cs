19a51fd66e275ddc5c361c62f886af9047689d83
MaizethC <119091973+MaizethC@users.noreply.github.com>
2025-03-28 17:23:19 -0600

    Update Allergy
    Changes were made to the allergy validations.

--- api/Dtos/Allergy/CreateAllergyRequestDto.cs
+++ api/Dtos/Allergy/CreateAllergyRequestDto.cs
@@ -10,12 +10,12 @@ namespace api.Dtos.Allergy
     {
         [Required(ErrorMessage = "El nombre es obligatorio.")]
         [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres.")]
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ,.:\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
         public string name { get; set; } = string.Empty;
        
         [Required(ErrorMessage = "La descripcion es obligatoria.")]
         [StringLength(255, ErrorMessage = "La descripción no puede tener más de 255 caracteres.")]
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "La descripción solo puede contener letras y espacios.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ,.:\s]+$" , ErrorMessage = "La descripción solo puede contener letras y espacios.")]
         public string? description { get; set; }
 
         public DateTime created_at { get; set; }
